66beefd2d9ba2d2f155a07aa9a5ef031d224fb0a
tcyang1971 <tcyang@gm.pu.edu.tw>
2025-09-15 15:30:08 +0800

    第二次commit (隨機產生樂透數字)

--- app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LottoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    Play(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +30,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Play(modifier: Modifier = Modifier) {
+    var lucky = (1..100).random()
+
     Text(
-        text = "Hello $name!",
+        text = "樂透數字(1-100)為 $lucky",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LottoTheme {
-        Greeting("Android")
-    }
 }
