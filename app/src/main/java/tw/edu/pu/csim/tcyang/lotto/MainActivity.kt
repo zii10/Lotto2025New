c3ce0d8126d3ebc746598001491b60f204067259
tcyang1971 <tcyang@gm.pu.edu.tw>
2025-09-15 16:32:04 +0800

    第四次commit (垂直排列)

--- app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -34,15 +35,18 @@ class MainActivity : ComponentActivity() {
 fun Play(modifier: Modifier = Modifier) {
     var lucky = (1..100).random()
 
-    Text(
-        text = "樂透數字(1-100)為 $lucky",
-        modifier = modifier
-    )
+    Column (modifier = modifier){
+        Text(
+            text = "樂透數字(1-100)為 $lucky"
+        )
 
-    Button(
-        onClick = { lucky = (1..100).random() }
-    ) {
-        Text("重新產生樂透碼")
+        Button(
+            onClick = { lucky = (1..100).random() }
+        ) {
+            Text("重新產生樂透碼")
+        }
     }
+
+
 
 }
