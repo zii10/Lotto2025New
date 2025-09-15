9ef4ae83df523791bfc56cbe2612bab4a2c3fb4f
tcyang1971 <tcyang@gm.pu.edu.tw>
2025-09-15 16:20:30 +0800

    第三次commit (加入按鈕)

--- app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/lotto/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -37,4 +38,11 @@ fun Play(modifier: Modifier = Modifier) {
         text = "樂透數字(1-100)為 $lucky",
         modifier = modifier
     )
+
+    Button(
+        onClick = { lucky = (1..100).random() }
+    ) {
+        Text("重新產生樂透碼")
+    }
+
 }
